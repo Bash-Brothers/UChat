594a6697591e1a38dea1fb7125df2acb1cc57e6e
Sudhanshu Agrawal <sudhanshuagrawal2001@gmail.com>
2021-03-01 04:19:32 -0800

    Page authentication enabled. Logging in changes the cookie state. Created a file util.jl which contains a isLoggedIn method that performs a fetch to backend /auth which checks the cookie and returns the appropriate boolean value. isLoggedIn() can be reused anywhere by importing it. isLoggedIn() is called during componentDidMount() for the protected pages and updates the state of the components accordingly. In the render() function of these components we then have a conditional redirect to /login if loggedIn = false. Implemented this for the /settings page and for the current /chats page

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import './style/ChatWindow.css';
 import IconSend from '../images/icon_send.svg';
+import {Redirect} from "react-router-dom";
+import {isLoggedIn} from '../utils.js';
+
 export default class ChatWindow extends Component {
 
+    constructor(props)
+    {
+        super(props);
+        this.state = {loggedIn: true};
+    }
+    componentDidMount() //we need to make sure we are actually logged in
+    {                   
+        console.log("Inside component did mount for chat window");
+        isLoggedIn().then(loggedIn => this.setState({loggedIn: loggedIn}));
+    }
+
     render() {
+        if(this.state.loggedIn == false)
+        {
+            return <Redirect to='/login' />;
+        }
         //contactList should be something that is received from the server
         const contactList = 'Yan,Kevin,Milo,Sud,Aman,Eggert,Eggboi,Eggs,SunnySideUp,Omelette,Sud,Aman,Eggert,Eggboi,Eggs,SunnySideUp,Omelette'.split(',');
 
         var renderedOutput = contactList.map(item => <div className="contact">{item}</div>)
-
+        console.log("inside chatwindow");
         return (
             <div className="chatWindow">
                 <div className="sidebar">

dd75a45451fb69cfdc27e809acfc66f7f926f036
Yan <ya2n@protonmail.com>
2021-03-05 12:05:33 +0800

    Friendrequests page integration of front-end and back-end by Yan

--- client/src/components/FriendRequests.js
+++ client/src/components/FriendRequests.js
@@ -6,20 +6,24 @@ import {isLoggedIn} from '../utils.js';
 
 
 
-function Friendrequest(props) {
+function FriendRequest(props) {
+
     return (
         <div className="friendrequest">
-            <form>
+            {/* https://stackoverflow.com/questions/42597602/react-onclick-pass-event-with-parameter */}
+            <form onSubmit={() => FriendRequests.handleSubmit(props)}>
                 <p class="friendname">{props.value}</p>
                 <input
                     type="submit"
                     class="acceptButton"
                     value="Accept"
+                    onChange = {FriendRequests.handleChange}
                 />
                 <input
                     type="submit"
                     class="deleteButton"
                     value="Delete"
+                    onChange = {FriendRequests.handleChange}
                 />
             </form>
         </div>
@@ -33,8 +37,78 @@ export default class FriendRequests extends Component {
     constructor(props)
     {
         super(props);
-        this.state = {loggedIn: true};
+        this.state = {
+            
+            // How do we get the current user's username?
+            username: "default",
+            loggedIn: true.valueOf,
+            friendreqList: ["Yan", "Kevin", "Milo", "Sud", "Aman"],
+            changedfriendrequest: "",
+            response: "none",
+
+            //contactList should be something that is received from the server
+            // const friendreqList = 'Yan,Kevin,Milo,Sud,Aman,Eggert,Eggboi,Eggs,SunnySideUp,Omelette,Sud,Aman,Eggert,Eggboi,Eggs,SunnySideUp,Omelette'.split(','),
+            
+            // More dummy strings
+            // , Eggert, Eggboi, Eggs, SunnySideUp, Omelette, Sud, Aman, Eggert, Eggboi, Eggs, SunnySideUp, Omelette
+        };
     }
+
+    handleChange = (event) => {
+        this.setState({response: event.target.value});
+    }
+
+
+
+    // For understanding of what is done here, look in to the concept of currying functions
+    // https://stackoverflow.com/questions/32782922/what-do-multiple-arrow-functions-mean-in-javascript
+    // https://stackoverflow.com/questions/60027202/how-do-i-pass-props-and-other-parameters-to-function-using-react-hooks
+    // https://stackoverflow.com/questions/60027202/how-do-i-pass-props-and-other-parameters-to-function-using-react-hooks
+    // https://stackoverflow.com/questions/42299594/await-is-a-reserved-word-error-inside-async-function
+
+    handleSubmit = props => async (event) => {
+
+        // No need for prevent default here
+        // event.preventDefault();
+
+        this.setState
+        ({
+            changedfriendrequest: props.value,
+        });
+
+        // alert('success');
+
+        const result = await fetch("/friendrequests", 
+        {
+            method: 'POST',
+            headers: 
+                {
+                    'Content-Type': "application/json; charset=utf-8",
+                },
+            // This is the data being posted
+            body: JSON.stringify(this.state) // + JSON.stringify(this.state.response)
+        })
+
+        
+        // To be uncommented:
+
+        // const res = await result.json();
+
+        // if(loggedIn)
+        // {
+        //     this.setState
+        //     ({
+        //         loggedIn: true
+        //     });
+        //     window.location.reload();
+        // }
+
+
+    }; 
+
+
+
+
     componentDidMount() //we need to make sure we are actually logged in
     {                   
         console.log("Inside component did mount for chat window");
@@ -42,8 +116,9 @@ export default class FriendRequests extends Component {
     }
 
     renderFriendrequest(item) {
+
         return (
-          <Friendrequest
+          <FriendRequest
             value={item}
           />
         );
@@ -54,10 +129,9 @@ export default class FriendRequests extends Component {
         {
             return <Redirect to='/login' />;
         }
-        //contactList should be something that is received from the server
-        const contactList = 'Yan,Kevin,Milo,Sud,Aman,Eggert,Eggboi,Eggs,SunnySideUp,Omelette,Sud,Aman,Eggert,Eggboi,Eggs,SunnySideUp,Omelette'.split(',');
 
-        var renderedOutput = contactList.map(item => this.renderFriendrequest(item))
+
+        var renderedOutput = this.state.friendreqList.map(item => this.renderFriendrequest(item))
         console.log("inside chatwindow");
         return (
             <div className="friendreqPage">
@@ -105,9 +179,6 @@ export default class FriendRequests extends Component {
             )
         }
     }
-
-
-
 
 
 

8e966f1a0e75477fc230d5ceda2a60994056d368
Monko2k <65096789+Monko2k@users.noreply.github.com>
2021-03-13 00:56:27 -0500

    App js cleaning (#59)
    * Cleaned App.js
    
    * Update latexRequest.py
    
    * More stuff

--- latexRequest.py
+++ latexRequest.py
@@ -4,13 +4,18 @@ import sys
 
 HOST = 'http://rtex.probablyaweb.site'
 
+# Get LaTeX code from stdin
 LATEX = sys.argv[1]
 
+# Submit POST request to rTeX
 payload = {'code': LATEX, 'format': 'png'}
 response = requests.post('http://rtex.probablyaweb.site/api/v2', data = payload)
 response.raise_for_status()
 jdata = response.json()
 if jdata['status'] != 'success':
 	raise Exception('Failed to render LaTeX')
+
+# Return link to resource on stdout
 url = HOST + '/api/v2/' + jdata['filename']
+
 sys.stdout.write(url)

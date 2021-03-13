764af59709c75dbd18d445605bcc4839ed6b80bb
Monko2k <65096789+Monko2k@users.noreply.github.com>
2021-03-13 00:49:42 -0500

    Cleaned App.js (#58)

--- client/src/App.js
+++ client/src/App.js
@@ -10,85 +10,17 @@ import About from './components/About.js';
 import SignupPage from './components/SignupPage.js';
 import SearchPage from './components/SearchPage.js';
 import FriendRequests from './components/FriendRequests.js';
-
 import Test from './components/Test.js';
 import Nav from './components/Nav.js';
 import Page404 from './components/Page404.js';
-
 import {isLoggedIn} from './utils.js';
 import {storage} from './firebase-config.js';
 
-// future features: notifications popup 
-// render the notifications menu if the user has clicked on the notifications button
-// function RenderNotifs(props) {
-//   const status = props.status;
-//   if (status) {
-//     return (
-//       <div class="notifications">
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//         <div class="notifications-content">
-//           notification
-//         </div>
-//       </div>
-//     )
-//   }
-//   return (null);
-// }
 
 //changed App from a function a class 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      curPage: 0,
-      showNotif: false,
-      response: '',
-    };
-    /*this.CurNav = this.CurNav.bind(this);
-    this.mainNav = this.mainNav.bind(this);
-    this.LogInNav = this.CurNav.bind(this);*/
-  }
-  //curPage values: Log in page: 0, Sign up page: 1, About page: 2, Settings page: 3, Chats page: 4
-  //loggedIn state variable is to make sure someone is logged in before they can click on settings or chats
 
-  handleClick(i) {
-    this.setState({
-      curPage: i,
-    })
-  }
-
-  handleLogin() {
-    this.setState({
-      curPage: 2,
-    })
-  }
-
-  handleNotifClick() {
-    this.setState({
-      showNotif: !this.state.showNotif,
-    })
-  }
-
+  // set up routing for every page within the app
   render() {
     return (
       <div className="App">
@@ -131,7 +63,7 @@ class App extends Component {
               <Route path="/friendrequests">
                 <FriendRequests />
               </Route>
-              <Route exact path="/"> // needs to be exact path otherwise it becomes default
+              <Route exact path="/"> {/* needs to be exact path otherwise it becomes default */}
                 <LoginPage/>
               </Route>
               <Route path="/404" component={Page404} />

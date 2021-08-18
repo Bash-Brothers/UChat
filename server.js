cb5d009f1645a9370b73c1c72fcae210f4a29188
Aman Oberoi <amanbir5103@gmail.com>
2021-08-17 22:09:25 -0700

    heroku v4

--- server.js
+++ server.js
@@ -956,11 +956,11 @@ app.listen(port, function () {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+    res.sendFile(path.join(__dirname, '/client/build'))
 })
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build'))
   });
 /*=========================================================================== 
 =========================== server.js END ===================================
